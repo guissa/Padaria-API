1d412bc5a6037e0d1507ccbb8e9d857fbc486515
Guilherme Batista <97068622+guissa@users.noreply.github.com>
2022-09-01 20:18:25 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from typing import Optional
+from fastapi import FastAPI
+import uvicorn
+import random
+
+app = FastAPI()
+
+@app.get("/")
+def hello_root():
+    return {"Mensagem": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
