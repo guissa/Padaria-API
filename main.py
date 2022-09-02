2828f4e9228da3cb787b427f0937137d14f3510a
Guilherme Batista <97068622+guissa@users.noreply.github.com>
2022-09-01 21:02:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 def hello_root():
-    return {"Mensagem": "Hello World"}
+    return {"Mensagem": "Eai Mario"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
