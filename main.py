681acaa812c0c32e545eda149cc7fd2c3e7a8712
Guilherme Batista <guissa04@icloud.com>
2022-09-01 21:23:50 -0300

    coloquei meu nome no projeto

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 @app.get("/")
 def hello_root():
-    return {"Mensagem": "Eai Mario"}
+    return {"Mensagem": "Eu sou o gui"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
