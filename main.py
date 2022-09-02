e93653164f0899f25b0fdef530092caba6feab9e
Guilherme Batista <guissa04@icloud.com>
2022-09-01 22:01:05 -0300

    =adicionando novos itens

--- main.py
+++ main.py
@@ -10,5 +10,15 @@ app = FastAPI()
 def hello_root():
     return {"Mensagem": "Eu sou o gui"}
 
+@app.get("/funcionarios")
+def hello_root():
+    funcionarios = [{"nome": "Guilherme Batista"}]
+    return funcionarios
+
+@app.get("/produtos")
+def hello_root():
+    produtos = [{"nome": "pao"}]
+    return produtos
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
