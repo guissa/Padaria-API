980be4326c025c9cbe548b70e065ebe06d26968a
Guilherme Batista <guissa04@icloud.com>
2022-09-08 16:03:30 -0300

    Projeto lista

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ from random import randint
 app = FastAPI()
 
 @app.get("/soma")
-async def sum(n1: int = 15, n2: int = 15):
-    return n1 + n2
+async def produtos(lista = ["pão","leite","agua"]):
+    resultado = produtos
+    return resultado
+ 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+
+app = FastAPI()
+
+
