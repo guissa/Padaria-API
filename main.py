e40440bddb24f676e367406c4911b20551737ceb
Guilherme Batista <guissa04@icloud.com>
2022-09-08 14:19:55 -0300

    coloquei meu nome no projeto

--- main.py
+++ main.py
@@ -2,23 +2,13 @@ import os
 from typing import Optional
 from fastapi import FastAPI
 import uvicorn
-import random
+from random import randint
 
 app = FastAPI()
 
-@app.get("/")
-def hello_root():
-    return {"Mensagem": "Eu sou o gui"}
-
-@app.get("/funcionarios")
-def hello_root():
-    funcionarios = [{"nome": "Guilherme Batista"}]
-    return funcionarios
-
-@app.get("/produtos")
-def hello_root():
-    produtos = [{"nome": "pao"}]
-    return produtos
+@app.get("/soma")
+async def sum(n1: int = 15, n2: int = 15):
+    return n1 + n2
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
